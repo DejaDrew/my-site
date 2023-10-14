e0750a1fd2308e1cd96f6d14b2684334c16a33b7
Andrew Erie <andrewerie@gmail.com>
2023-10-13 22:12:48 -0600

    Adjust spacing

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,17 @@ import About from "@/components/about";
 import Contact from "@/components/contact";
 import Experience from "@/components/experience";
 import Intro from "@/components/intro";
+import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center ">
       <Intro />  
-      <About />    
+      <About />
+      <SectionDivider />    
       <Skills />
+      <SectionDivider />  
       <Experience />      
       <Contact />
     </main>
